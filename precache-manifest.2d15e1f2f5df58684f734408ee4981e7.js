49249b77f3b4c0358335f8d3c3eebb87e1052bfe
Rafael Nogueira <rafael@drupalized.co>
2020-03-31 20:09:15 -0400

    First commit

---
+++ precache-manifest.2d15e1f2f5df58684f734408ee4981e7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c08c9e9e923c2820fd44b8bf767ffcc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6b86c5339e3ef27d346b",
+    "url": "/static/css/main.9362dcfe.chunk.css"
+  },
+  {
+    "revision": "0066aad6b729d42a24c6",
+    "url": "/static/js/2.dea334b4.chunk.js"
+  },
+  {
+    "revision": "174585a64b344d550d2bfa36ad73cd69",
+    "url": "/static/js/2.dea334b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b86c5339e3ef27d346b",
+    "url": "/static/js/main.68e7e6b5.chunk.js"
+  },
+  {
+    "revision": "4889f2cdbab0c6d2c385",
+    "url": "/static/js/runtime-main.c31f78c7.js"
+  },
+  {
+    "revision": "71a9c378e9602e14f11a43c9696a116f",
+    "url": "/static/media/logo.71a9c378.svg"
+  }
+]);
